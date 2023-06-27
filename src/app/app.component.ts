477b33cb7f4152c729271b1195cc445c8c90e513
jakubsuchanowski <kubasuchanowski@gmail.com>
2023-06-27 23:55:45 +0200

    v2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AppService} from "./app.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'frontend';
+  email = '';
+  password = '';
+  data: Data[];
+  ngOnInit() {
+  }
+
+  constructor(private appService: AppService) {
+    this.data = [];
+  }
+
+  login() {
+    console.log('email' + this.email);
+
+    let payload = {email: this.email, password: this.password};
+
+    this.appService.loginApi(payload).subscribe((res: any) => {
+      console.log(res)
+      this.getData(res.accessToken);
+    })
+  }
+
+  getData(token: string) {
+    this.appService.getData(token).subscribe((res: any) => {
+      console.log(res);
+      this.data=res;
+    })
+  }
+}
+
+export interface Data{
+  title: string;
+  author: string;
 }
